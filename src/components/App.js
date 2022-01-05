6aaed7ff5930ff394105a91be7f83b4e4e27b44e
Celdama <celdama.dev@gmail.com>
2022-01-05 12:55:18 +0100

    everything is ok, build work. Last commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const App = () => {
-  return (
-    <div>
-      <h3>Hello World!</h3>
-    </div>
-  );
-};
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello World!</h1>
+      </div>
+    );
+  }
+}
 
 export default App;
